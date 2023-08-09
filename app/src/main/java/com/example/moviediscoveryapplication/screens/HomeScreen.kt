134cf7f6cabf7b466c5a0c8c39539af2d47a3651
Ricardo Ribeiro <ricardoribeiro@Macbook-de-Ricardo.local>
2023-08-09 16:48:43 -0300

    profile section of the home screen done

--- app/src/main/java/com/example/moviediscoveryapplication/screens/HomeScreen.kt
+++ app/src/main/java/com/example/moviediscoveryapplication/screens/HomeScreen.kt
@@ -1,36 +1,94 @@
 package com.example.moviediscoveryapplication.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviediscoveryapplication.R
+
+object ProfileStrings {
+    const val HELLO_TEXT = "Hello, Smith"
+    const val STREAM_TEXT = "Let’s stream your favorite movie"
+}
 
 @Composable
 fun HomeScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
-        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "HOME",
-            fontSize = MaterialTheme.typography.headlineMedium.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Column {
+            ProfileSection()
+        }
     }
 }
 
 @Composable
+fun ProfileSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(48.dp)
+                .clip(RoundedCornerShape(size = 180.dp))
+                .background(Color.LightGray),
+            painter = painterResource(id = R.drawable.ic_profile),
+            contentDescription = "circular image profile"
+        )
+
+        Column {
+            Text(
+                text = ProfileStrings.HELLO_TEXT,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+            )
+
+            CompositionLocalProvider(LocalContentColor provides Color.LightGray) {
+                Text(
+                    text = ProfileStrings.STREAM_TEXT,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = MaterialTheme.typography.labelMedium.fontSize,
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
 @Preview
 fun HomeScreenPreview() {
-    HomeScreen()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column {
+            ProfileSection()
+        }
+    }
 }
