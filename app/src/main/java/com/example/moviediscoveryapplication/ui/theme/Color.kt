9fd7321f18105b7383bc3de7f04d1ad23c2d3549
Ricardo Ribeiro <ricardoribeiro@Macbook-de-Ricardo.local>
2023-08-09 12:38:18 -0300

    defining colors and theme

---
+++ app/src/main/java/com/example/moviediscoveryapplication/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.moviediscoveryapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlue = Color(0xFF1F1D2B)
+val SoftBlue = Color(0xFF252836)
+val Cyan = Color(0xFF12CDD9)
+
+
+val Red = Color(0xFFFB4141)
+val Orange = Color(0xFFFF8700)
+val Green = Color(0xFF22B07D)
